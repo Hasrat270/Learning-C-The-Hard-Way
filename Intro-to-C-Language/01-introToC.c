a285997e7c045ace6998520bdcc90eed62b60ade
Hasrat270 <hasrat3701@gmail.com>
2026-04-12 02:26:27 +0500

    learning c

---
+++ Intro-to-C-Language/01-introToC.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* Learning C the hard way */
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+	// This is a variable
+	printf("You are %d miles away.\n", distance);
+	return 0;
+}
