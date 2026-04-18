877a45b9b60140c44c04a5f2b52f9baa600a8001
Hasrat270 <hasrat3701@gmail.com>
2026-04-18 19:19:43 +0500

    journey contiues

--- 07-WhileLoopAndBool.c
+++ 07-WhileLoopAndBool.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    int i = 25;
-    while (i >= 1) {
-        printf("%d ", i);
-        i--;
+int main(int argc, char* argv[]) {
+    int num = 25;
+    while (num <= 25 && num >= 1) {
+        if (num == 25) {
+            printf("State %d: Karachi\n", num);
+        } else if (num == 24) {
+            printf("State %d: Lahore\n", num);
+        } else {
+            printf("Num %d:\n", num);
+        }
+        num--;
     }
     return 0;
 }
